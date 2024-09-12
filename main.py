32b4788fe99c8f6801bc424aa4809037ee7cf093
shalunbdk <shalunbdk@gmail.com>
2024-09-12 14:42:58 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, Query, Body
+from fastapi.openapi.docs import get_swagger_ui_html
+import uvicorn
+
+app = FastAPI()
+
+hotels = [
+    {"id": 1, "title": "Sochi", "name":"sochi"},
+    {"id": 2, "title": "Дубай", "name":"dubai"},
+]
+
+@app.get("/hotels")
+def get_hotels(
+    id: int | None = Query(None, description="Айди отеля"),
+    title: str | None = Query(None, description="Название отеля"),
+):
+    hotels_ = []
+    for hotel in hotels:
+        if id and hotel["id"] != id:
+            continue
+        if title and hotel["title"] != title:
+            continue
+        hotels_.append(hotel)
+    return hotels_
+
+@app.delete("/hotels/{hotel_id}")
+def delete_hotel(hotel_id: int):
+    global hotels
+    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
+    return {"status":"ok"}
+
+@app.patch("/hotels/{hotel_id}")
+def patch_hotel(
+    hotel_id: int,
+    title: str | None = Body(default=None),
+    name: str | None = Body(default=None)
+):
+    global hotel
+    for hotel in hotels:
+        if(hotel["id"] == hotel_id):
+            if title:
+                hotel["title"] = title
+            if name:
+                hotel["name"] = name
+    return {"status":"ok"}
+
+@app.put("/hotels/{hotel_id}")
+def put_hotel(
+    hotel_id: int,
+    title:str = Body(embed=True),
+    name:str = Body(embed=True)
+):
+    global hotels
+    for hotel in hotels:
+        if(hotel["id"] == hotel_id):
+            hotel["title"] = title
+            hotel["name"] = name
+    return {"status":"ok"}
+
+@app.post("/hotels")
+def create_hotel(
+    title:str = Body(embed=True)
+):
+    global hotels
+    hotels.append({
+        "id":hotels[-1]["id"] + 1,
+    })
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
