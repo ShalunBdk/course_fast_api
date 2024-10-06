0f4a1e0afe5f2d1640eb1214a894312ffa384db2
shalunbdk <shalunbdk@gmail.com>
2024-10-06 19:42:03 +0700

    add facilities to one_or_none room get

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -29,3 +29,11 @@ class RoomsRepository(BaseRepository):
         )
         result = await self.session.execute(query)
         return [RoomWithRels.model_validate(model, from_attributes=True) for model in result.scalars().all()]
+    
+    async def get_one_or_none(self, **filer_by):
+        query = select(self.model).filter_by(**filer_by).options(selectinload(self.model.facilities))
+        result = await self.session.execute(query)
+        model = result.scalars().one_or_none()
+        if model is None:
+            return None
+        return RoomWithRels.model_validate(model, from_attributes=True)
